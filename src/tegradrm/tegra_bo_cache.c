digetx/libvdpau-tegra

commit 754fb7fe13c844b6fa55ec79676b3da86e02f7ba

tegradrm: tune up caching parameters
Keep BO in cache longer to prevent frequent re-allocations that may take
too much time.

src/tegradrm/tegra_bo_cache.c
@@ -131,12 +131,16 @@ void drm_tegra_bo_cache_cleanup(struct drm_tegra *drm, time_t time)
 
 			delta = time - bo->free_time;
 
-			/* keep things in cache for at least 1 second: */
-			if (time && delta <= 1)
+			/* keep things in cache for at least 30 second: */
+			if (time && delta <= 30)
 				break;
 
 			/* keep things in cache longer if not much */
 			if (time && delta < 60 && bucket->num_entries < 5)
+				break;
+
+			if (time && delta < 300 &&
+			    bucket->size * bucket->num_entries < 4 * 1024 * 1024)
 				break;
 
 			VG_BO_OBTAIN(bo);
@@ -193,7 +197,7 @@ static bool is_idle(struct drm_tegra_bo *bo)
 }
 
 static struct drm_tegra_bo *find_in_bucket(struct drm_tegra_bo_bucket *bucket,
-					   uint32_t flags)
+					   uint32_t flags, uint32_t size)
 {
 	struct drm_tegra_bo *bo = NULL;
 
@@ -207,6 +211,7 @@ static struct drm_tegra_bo *find_in_bucket(struct drm_tegra_bo_bucket *bucket,
 	if (!DRMLISTEMPTY(&bucket->list)) {
 		bo = DRMLISTENTRY(struct drm_tegra_bo, bucket->list.next,
 				  bo_list);
+
 		/* TODO check for compatible flags? */
 		if (is_idle(bo)) {
 			DRMLISTDELINIT(&bo->bo_list);
@@ -267,8 +272,7 @@ drm_tegra_bo_cache_alloc(struct drm_tegra *drm,
 
 	/* see if we can be green and recycle: */
 	if (bucket) {
-		*size = bucket->size;
-		bo = find_in_bucket(bucket, flags);
+		bo = find_in_bucket(bucket, flags, *size);
 		if (bo) {
 			drm_tegra_reset_bo(bo, flags, false);
 
@@ -341,8 +345,8 @@ drm_tegra_bo_mmap_cache_cleanup(struct drm_tegra *drm,
 
 		delta = time - bo->unmap_time;
 
-		/* keep things in cache for at least 3 seconds: */
-		if (time && delta <= 3)
+		/* keep things in cache for at least 30 seconds: */
+		if (time && delta <= 30)
 			break;
 
 		if (time) {

commit f08abc5ec1ea59f1c763dd11a7e6521abe8a0b23

tegradrm: fix bo size set by drm_tegra_bo_cache_alloc()
Commit 754fb7fe13c8 accidentally touched unrelated code and BO size
isn't set properly now, causing memory corruption, fix it.

src/tegradrm/tegra_bo_cache.c
@@ -197,7 +197,7 @@ static bool is_idle(struct drm_tegra_bo *bo)
 }
 
 static struct drm_tegra_bo *find_in_bucket(struct drm_tegra_bo_bucket *bucket,
-					   uint32_t flags, uint32_t size)
+					   uint32_t flags)
 {
 	struct drm_tegra_bo *bo = NULL;
 
@@ -272,7 +272,8 @@ drm_tegra_bo_cache_alloc(struct drm_tegra *drm,
 
 	/* see if we can be green and recycle: */
 	if (bucket) {
-		bo = find_in_bucket(bucket, flags, *size);
+		*size = bucket->size;
+		bo = find_in_bucket(bucket, flags);
 		if (bo) {
 			drm_tegra_reset_bo(bo, flags, false);
 